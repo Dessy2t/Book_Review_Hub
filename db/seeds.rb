e7cd3b163b93477feeef401b05bee81060550079
Desmond Yentumi <yentumid7@gmail.com>
2025-03-21 13:18:33 +0000

    Created models and database structure for books and reviews with their relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Book.create([
+  { title: "Sample Book 1", author: "Author One", isbn: "123-4567890123" },
+  { title: "Sample Book 2", author: "Author Two", isbn: "234-5678901234" },
+  { title: "Sample Book 3", author: "Author Three", isbn: "345-6789012345" }
+])
+
+Review.create([
+  { content: "Great book!", rating: 5, book_id: 1 },
+  { content: "Interesting read.", rating: 4, book_id: 1 },
+  { content: "Not my taste.", rating: 2, book_id: 2 },
+  { content: "Loved it!", rating: 5, book_id: 2 },
+  { content: "Okay.", rating: 3, book_id: 3 }
+])
