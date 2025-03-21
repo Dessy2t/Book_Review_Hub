15c3fdf30cd04c8ad94d8d41c5b034f53e1c0f9a
Desmond Yentumi <yentumid7@gmail.com>
2025-03-21 14:09:34 +0000

    Books managing controller created.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.build(review_params)
+  if @review.save
+    redirect_to @book, notice: "Review added!"
+  else
+    redirect_to @book, alert: "Error adding review."
+  end
+end
+def destroy
+  @review = Review.find(params[:id])
+  @review.destroy
+  redirect_to @review.book
+end
+private
+def review_params
+  params.require(:review).permit(:content, :rating)
+end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,24 @@
+require 'net/http'
+require 'json'
+
 class Book < ApplicationRecord
+
+  def self.find_by_isbn(isbn)
+    url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn}&format=json&jscmd=data"
+    response = Net::HTTP.get(URI(url))
+    data = JSON.parse(response)
+
+    book_data = data["ISBN:#{isbn}"]
+    return nil unless book_data
+
+    {
+      title: book_data["title"],
+      author: book_data["authors"]&.first["name"],
+      isbn: isbn
+    }
+  end
+
+
   has_many :reviews, dependent: :destroy
   has_one_attached :cover_image
   
